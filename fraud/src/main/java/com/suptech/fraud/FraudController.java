8c4678546309ce4767c595a88ee99d0439274889
chebihiF <chebihi.f@ucd.ac.ma>
2022-11-21 21:05:16 +0100

    version 1.0

--- fraud/src/main/java/com/suptech/fraud/FraudController.java
+++ fraud/src/main/java/com/suptech/fraud/FraudController.java
@@ -1,6 +1,7 @@
 package com.suptech.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 @RequestMapping("api/v1/fraud-check")
 public class FraudController {
     private FraudCheckService fraudCheckService;
@@ -15,6 +17,7 @@ public class FraudController {
     @GetMapping(path = "{customer_id}")
     public FraudCheckResponse isFraudster(@PathVariable Integer customer_id){
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customer_id);
+        log.info("fraud check request for customer {}", customer_id);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
